73422d88728efd9e1ce265144bd3de2921d4a884
HajnalFlorian <32631861+HajnalFlorian@users.noreply.github.com>
2022-03-30 19:00:54 +0200

    RandoAddict

---
+++ randoaddict_DML.sql
@@ -0,0 +1,15 @@
+insert into EXCURSION values
+	(1,'Le tour du Mont-Blanc','Randonnée parmi les plus belles du monde, le tour du Mont-Blanc se joue des frontières car il traverse les richesses françaises, suisses et italiennes d’un massif mythique.',895,,5,'foret','Tours','Villars'),
+
+
+insert into MARCHEUR values
+	(1,'IceAgfe','Buck',33,'0601020301'),
+	(2,'LionKing','TimonPumpa',23,'0682734411'),
+	(3,'Mystere','SamyScooby',27,'0634996133'),
+	(4,'USA','Sylvestre',44,'0678418799'),
+
+
+
+insert into PROGRAMME values
+	(1,'','2022/05/04','2022/05/08',1,1),
+
